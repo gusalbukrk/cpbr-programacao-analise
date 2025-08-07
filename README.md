2e86bcd68aa10bf811a1c02eb5c68298d30d24c4
gusalbukrk <gusalbukrk@gmail.com>
2025-08-07 16:37:38 -0300

    make `categorizar.py` async, edit `README`

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# Programação da Campus Party Brasil: Uma Análise das Últimas 10 Edições
+# Campus Party Brasil: Uma análise da programação das 10 edições realizadas entre 2022 e 2024
 
 - A [versão 1](https://github.com/gusalbukrk/cpbr-programacao-analise/commit/64790beaa47dd29a6e3ba9b89259cc7c15550133) deste projeto foi apresentada em um artigo científico, na 10ª edição da revista da Campus Party Brasil, disponível [aqui](https://brasil.campus-party.org/revista-cientifica/).
-- A [versão 2](https://github.com/gusalbukrk/cpbr-programacao-analise/commit/a6605d0b3b52481511e8322610b87feb5e3d33fc) deste projeto foi apresentada na CPBR17, conforme divulgado [aqui](https://app.4.events/palestrante-Luiz-Gustavo-Albuquerque-6117-c18443).
+- A [versão 2](https://github.com/gusalbukrk/cpbr-programacao-analise/commit/a6605d0b3b52481511e8322610b87feb5e3d33fc) deste projeto foi apresentada em uma palestra na CPBR17, conforme divulgado [aqui](https://app.4.events/palestrante-Luiz-Gustavo-Albuquerque-6117-c18443).
+
+## Modelos do Gemini utilizados
+
+- [v1](https://github.com/gusalbukrk/cpbr-programacao-analise/commit/64790beaa47dd29a6e3ba9b89259cc7c15550133): `gemini-2.0-flash-lite`
+- [v2](https://github.com/gusalbukrk/cpbr-programacao-analise/commit/a6605d0b3b52481511e8322610b87feb5e3d33fc): `gemini-2.5-flash-preview-04-17`
+
+## Como rodar
+
+- `python -m venv .venv`
+- `source .venv/bin/activate`
+- `pip install -r requirements.txt`
+- para executar o script `categorizar.py`, crie o arquivo `.env` na raiz do projeto contendo a variável `API_KEY` com a sua chave da API do Gemini
+- para visualizar o dashboard, execute `streamlit run src/visualizar.py`
 
 ## Como citar
 
@@ -9,17 +22,10 @@ Se você utilizar o código ou o dataset deste repositório em seu trabalho, por
 
 ```bibtex
 @misc{albuquerque2025,
-  title={{Programação da Campus Party Brasil: Uma Análise das Últimas 10 Edições}},
+  title={{Campus Party Brasil: Uma análise da programação das 10 edições realizadas entre 2022 e 2024}},
   author={{Luiz Gustavo Albuquerque}},
   year={2025},
   url={https://github.com/gusalbukrk/cpbr-programacao-analise},
   notes={Código e dataset}
 }
 ```
-
-## Como rodar
-
-- `python -m venv .venv`
-- `source .venv/bin/activate`
-- `pip install -r requirements.txt`
-- create a `.env` file containing a `API_KEY` variable with your Gemini API key
--- src/categorizar.py
+++ src/categorizar.py
@@ -9,13 +9,16 @@ from google import genai
 import re
 import pandas as pd
 from collections import Counter, defaultdict
+import asyncio
+from aiolimiter import AsyncLimiter
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
 categorizado_caminho = os.path.join(src_dir, "..", "data", "categorizado.json")
 
 load_dotenv()
 
-ai = genai.Client(api_key=os.getenv("API_KEY"))
+aiClient = genai.Client(api_key=os.getenv("API_KEY"))
 
 with open(os.path.join(src_dir, "..", "prompt_template.md"), "r", encoding="utf-8") as f:
     prompt_template = f.read()
@@ -47,7 +50,7 @@ def remover_markdown_code_blocks(text):
 
 def obter_horario_atual():
     agora = datetime.now()
-    return agora.strftime("%H:%M:%S")
+    return agora.strftime("%H:%M:%S:%f")
 
 def is_in(s, l):
     return s.lower() in [ el.lower() for el in l ]
@@ -102,13 +105,14 @@ def consolidar_candidatos(candidatos):
 
     return consolidado
 
-def categorizar(atividade):
+@retry(stop=stop_after_attempt(5), wait=wait_fixed(30))
+async def categorizar(atividade):
     prompt = f"{prompt_template}{omitir_atributos_irrelevantes(atividade).to_json(force_ascii=False)}"
 
     try:
-        resp = ai.models.generate_content(
-            # model="gemini-2.0-flash-lite",
-            model="gemini-2.5-flash-preview-04-17",
+        resp = await aiClient.aio.models.generate_content(
+            model="gemini-2.5-flash",
+            # model="gemini-2.5-flash-lite",
             contents=prompt,
             config=genai.types.GenerateContentConfig(
                 candidateCount=3,
@@ -119,37 +123,65 @@ def categorizar(atividade):
             json.loads(remover_markdown_code_blocks(candidate.content.parts[0].text))
             for candidate in resp.candidates
         ]
-    except Exception:
-        return None
+    except Exception as e:
+        print(f'{obter_horario_atual()} - API-related error:', e)
+        raise e
 
     consolidado = consolidar_candidatos(candidatos)
 
     return consolidado
 
-def main():
+async def processar(atividade, atividade_index, atividades_processadas, atividades_quant_total, rate_limit, lock):
+
+    if (atividade.uid not in atividades_processadas["uid"].values):
+        atividade_already_processed = False
+
+        async with rate_limit:
+            print(f"{obter_horario_atual()} - {atividade_index + 1} de {atividades_quant_total} (started)")
+
+            categorias = await categorizar(atividade)
+
+            # after using `.loc()`, the dtype of the uid columns changes from category to object
+            # therefore, must reconfigure order
+            uids_ordered = atividades_processadas['uid'].cat.categories
+            atividades_processadas.loc[len(atividades_processadas)] = {**atividade, **categorias}
+            atividades_processadas['uid'] = pd.Categorical(atividades_processadas['uid'], categories=uids_ordered, ordered=True)
+    else:
+        atividade_already_processed = True
+
+    async with lock:
+        atividades_processadas = atividades_processadas.sort_values(by='uid')
+        atividades_processadas.to_json(categorizado_caminho, orient='records', force_ascii=False, indent=2)
+
+    print(f"{obter_horario_atual()} - {atividade_index + 1} de {atividades_quant_total} ({ "already done" if atividade_already_processed else "finished" })")
+
+async def main():
     columns = ['uid', 'edicao', 'name', 'auditorium', 'description', 'open', 'start_time', 'end_time', 'speakers', 'eixo', 'perfil_organizador', 'formato', 'objetivo']
     
     limpo = pd.read_json(os.path.join(src_dir, "..", "data", "limpo.json"))
+    uids_ordered = list(limpo['uid'])
+    limpo['uid'] = pd.Categorical(limpo['uid'], categories=uids_ordered, ordered=True)
 
     try:
-        categorizado = pd.read_json(categorizado_caminho)
-    except Exception:
-        categorizado = pd.DataFrame(columns=columns)
+        atividades_processadas = pd.read_json(categorizado_caminho)
+    except Exception: # if it doesn't exist yet
+        atividades_processadas = pd.DataFrame(columns=columns)
+
+    atividades_processadas['uid'] = pd.Categorical(atividades_processadas['uid'], categories=uids_ordered, ordered=True)
 
     atividades_quant_total = len(limpo)
 
-    for _, atividade in limpo.iterrows():
-        if (atividade.uid in categorizado["uid"].values):
-            categorizado.to_json(categorizado_caminho, orient='records', force_ascii=False, indent=2)
-            continue
+    # send at one request at every 6.5 seconds
+    # to ensure it remains under the limit of 10 RPM
+    rate_limit = AsyncLimiter(1, 6.5)
 
-        print(f"{obter_horario_atual()} - {len(categorizado) + 1} de {atividades_quant_total}")
+    # used to lock writing to `categorizado.json`
+    # to assure file is not corrupted due to race conditions
+    lock = asyncio.Lock()
 
-        while ((categorias := categorizar(atividade)) == None):
-            pass
-
-        categorizado.loc[len(categorizado)] = {**atividade, **categorias}
-        categorizado.to_json(categorizado_caminho, orient='records', force_ascii=False, indent=2)
+    atividades = [ atividade for _, atividade in limpo.iterrows() ]
+    tasks = [ processar(atividade, atividade_index, atividades_processadas, atividades_quant_total, rate_limit, lock) for atividade_index, atividade in enumerate(atividades) ]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
